ea89ce4e3b40ec5824c0ea0bba901a5e6722c0df
Wahib Irawan <53545204+ez36@users.noreply.github.com>
2020-02-09 13:09:23 +0700

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -42,7 +42,7 @@ class Twitter:
                     dms.reverse()
 
             print(str(len(dms)) + " collected")
-            time.sleep(60)
+            time.sleep(40)
             return dms
 
         except Exception as ex:
